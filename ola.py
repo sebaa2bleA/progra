70f0a7abd5c8b31ee264901e6fe12db1d7f84e95
sebaa2bleA <sebaa2bleA@users.noreply.github.com>
2023-05-03 16:17:30 -0400

    subiendo nuevo archivo prueba

--- ola.py
+++ ola.py
@@ -1,1 +1,9 @@
 print("ola mundo")
+##Cuando creo un repositorio nuevo
+##git init
+##git add .
+##git commit -m "Subiendo archivo"
+##git branch -m main
+##git remote add origin (URL que puede cambiar) https://github.com/sebaa2bleA/test.git
+##git pull origin main
+##git push -u origin main
